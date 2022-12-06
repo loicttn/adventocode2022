0b8112ba5c4606c65b83abb6b0343030c0315f9b
pwnh4 (@loicttn) <loic@ttn.am>
2022-12-06 06:14:20 +0100

    feat: day6

---
+++ day6/2.py
@@ -0,0 +1,19 @@
+def solve(d):
+    x = d[0]
+    i = 0
+    while i < len(x):
+        buff = x[i:i+14]
+        if "".join(sorted(buff)) == "".join(sorted(list(set(buff)))):
+            print(i + 14)
+            break
+        i += 1
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        c = f.read().split("\n")
+        solve(c)
+    with open("ex.txt", "r") as f:
+        c = f.read().split("\n")
+        solve(c)
+       
